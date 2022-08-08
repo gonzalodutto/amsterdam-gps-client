29b861312c57f2eb2848c3df449f3c5b9d431c51
gonzalodutto <gonzalodutto@gmail.com>
2022-08-08 16:58:16 +0200

    pages components created

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,22 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import "./styles.css";
+
+const NavBar = (props) => {
+  return (
+    <div className="NavContainer">
+      <div className="NavLinksAndInput">
+        <div className="NavLinks">
+          <NavLink to="/">Home</NavLink>
+          {" | "}
+          <NavLink to="/doctor_schedule">Doctor Schedule</NavLink>
+          {" | "}
+          <NavLink to="/patient_signup">Patient SignUp</NavLink>
+          {" | "}
+          <NavLink to="/patient_database">Patient Database</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { NavBar };
